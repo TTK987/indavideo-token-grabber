1aaa4c0b7eae89aff6c8e189685d02266e63e8b4
TTK987 <99139882+TTK987@users.noreply.github.com>
2024-04-04 03:20:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+"""
+Simple Indavideo.hu embed token generator
+
+Logic from yt_dlp ( github.com/yt-dlp/yt-dlp )
+
+"""
+
+# Required Libraries
+import urllib.parse
+import requests
+import re
+import time
+
+# Function to generate Indavideo token
+def generate_indavideo_token(url: str):
+    """
+    Generate urls to video files with token for indavideo.hu embed URLs
+    :param url: indavideo URL (embed or video page)
+    :return: Dictionary of urls to video files with token {height <int>: url <str>}
+    """
+    # Extract video ID from URL
+    video_id = re.search(r'https?://(?:(?:embed\.)?indavideo\.hu/player/video/|assets\.indavideo\.hu/swf/player\.swf\?.*\b(\?:v(?:ID|id))=)(?P<id>[\da-f]+)', url).group('id')
+    # Get video data
+    response = requests.get(
+        f'https://amfphp.indavideo.hu/SYm0json.php/player.playerHandler.getVideoData/{video_id}/',
+        params={'_': int(time.time())}
+    )
+    # Extract video data
+    video = response.json()['data']
+    # Extract video files
+    video_files = video.get('video_files', [])
+    # Check if video files is a dictionary
+    if isinstance(video_files, dict):
+        # Convert dictionary to list
+        video_files = list(video_files.values())
+    # Extract tokens from video data
+    filesh = video.get('filesh', {})
+    # Initialize tokens dictionary
+    tokens = {}
+    # Iterate over video files
+    for video_url in video_files:
+        # Extract height from video URL
+        height = re.search(r'.(\d{3,4}).mp4(?:\?|$)', video_url)
+        # Check if height is found
+        if height:
+            height = height.group(1)
+        token = filesh.get(str(height))
+        if token:
+            url_parts = list(urllib.parse.urlparse(video_url))
+            query = dict(urllib.parse.parse_qsl(url_parts[4]))
+            query.update({'token': token})
+            url_parts[4] = urllib.parse.urlencode(query)
+            tokens[height] = urllib.parse.urlunparse(url_parts)
+
+    return tokens
+
+print(generate_indavideo_token('https://embed.indavideo.hu/player/video/eac6d340e3?autostart=1&hide=titleshare&hq=1')) # Example URL
